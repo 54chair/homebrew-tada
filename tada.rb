52b18b946283d951b86548927fcd5291db32cd45
54chair <takukoshi3721@gmail.com>
2024-07-24 08:23:42 +0900

    タグのバージョンを修正

--- tada.rb
+++ tada.rb
@@ -1,7 +1,7 @@
 class Tada < Formula
   desc "ドン！"
   homepage ""
-  url "https://github.com/54chair/tada/archive/refs/tags/0.0.1.tar.gz"
+  url "https://github.com/54chair/tada/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   depends_on "php"
