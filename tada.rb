38d9e2405a91700f96f83d843c2e5338fbd59b33
54chair <takukoshi3721@gmail.com>
2024-07-24 08:08:47 +0900

    Initial commit

---
+++ tada.rb
@@ -0,0 +1,16 @@
+class Tada < Formula
+  desc "ドン！"
+  homepage ""
+  url "https://github.com/54chair/tada/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
+  depends_on "php"
+
+  def install
+    bin.install "tada.php" => "tada"
+  end
+
+  # test do
+  #   system "#{bin}/tada"
+  # end
+end
