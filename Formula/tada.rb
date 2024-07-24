26fbd49e9b5d40072bfe1faab22530f5d52f53eb
54chair <takukoshi3721@gmail.com>
2024-07-24 20:08:09 +0900

    v0.0.2へのアップデートに伴いsha256を変更

--- Formula/tada.rb
+++ Formula/tada.rb
@@ -1,8 +1,8 @@
 class Tada < Formula
   desc "ドン！"
   homepage ""
-  url "https://github.com/54chair/tada/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "66e234faeb579ab856dea20de5e17b835807fcade35b8960680d265812f059c7"
+  url "https://github.com/54chair/tada/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "03acfd4187f6626f49e828908da4e722c64347f5f3d04a98a9887e103185a239"
 
   depends_on "php"
 
